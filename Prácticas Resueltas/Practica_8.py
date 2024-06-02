0dfeb7045ed7f8ce4c201429196a66d5ffe195b8
Fran Pucciarelli <franciscopucciarelli99@gmail.com>
2024-06-02 01:07:20 -0300

    Ejercicios 7, 8, 9 y 10 de la Práctica 8 resueltos. También renombré algunos archivos (con los que realizo las pruebas).

--- Prácticas Resueltas/Practica_8.py
+++ Prácticas Resueltas/Practica_8.py
@@ -1,5 +1,8 @@
 # Práctica 8 - Archivos, Pilas, Colas y Diccionarios.
 
+import random 
+from queue import LifoQueue as Pila      # Librería para trabar con listas LIFO bajo la 
+                                         # denominación 'Pila'.
 
 print ('Práctica 8')
 print ('Ejercicio ')
@@ -13,7 +16,7 @@ def contar_lineas (nombre_archivo:str) -> int :
     archivo.close() 
     return len(lista_lineas) 
 
-# print (contar_lineas ('Pruebas Practica 8.txt')) 
+# print (contar_lineas ('Pruebas - Practica 8.txt')) 
 
 # 1.2 
 def palabra_contenida (palabra:str , linea:str) -> bool : 
@@ -49,9 +52,9 @@ def existe_palabra (palabra:str , nombre_archivo:str) -> bool :
             None 
     return res 
 
-# print (existe_palabra ('avion','Pruebas Practica 8.txt'))
-# print (existe_palabra ('refieren','Pruebas Practica 8.txt')) 
-# print (existe_palabra (',','Pruebas Practica 8.txt'))
+# print (existe_palabra ('avion','Pruebas - Practica 8.txt'))
+# print (existe_palabra ('refieren','Pruebas - Practica 8.txt')) 
+# print (existe_palabra (',','Pruebas - Practica 8.txt'))
 
 # 1.3 
 # Para esta resolución, considero PALABRA = conjunto de caracteres
@@ -93,9 +96,9 @@ def cantidad_apariciones (nombre_archivo:str , palabra:str) -> int :
                 None 
     return apariciones 
 
-# print (cantidad_apariciones ('Pruebas Practica 8.txt','avion'))
-# print (cantidad_apariciones ('Pruebas.txt','Archivo'))
-# print (cantidad_apariciones ('Pruebas.txt','muro'))
+# print (cantidad_apariciones ('Pruebas - Practica 8.txt','avion'))
+# print (cantidad_apariciones ('Pruebas - Practica 8.txt','Archivo'))
+# print (cantidad_apariciones ('Pruebas - Practica 8.txt','muro'))
 
 #! Esta función NO FUNCIONA cuando PALABRA es una PALABRA CON VOCAL TILDADA.
 #! Inicié el Debugger (para ver qué estaba pasando) y noté que cuando define 
@@ -161,7 +164,7 @@ def clonar_sin_comentarios (nombre_archivo:str) -> None :
     archivo2.writelines(lista_lineas_no_comentarios) 
     archivo2.close()
 
-# clonar_sin_comentarios ('Pruebas Practica 8.txt') 
+# clonar_sin_comentarios ('Pruebas - Practica 8.txt') 
 
 def clonar_sin_comentarios_2 (nombre_archivo:str) -> str : 
     archivo = open(nombre_archivo,'r')
@@ -195,7 +198,7 @@ def invertir_lineas (nombre_archivo:str) -> None :
     archivo2.writelines(reverso_lista_lineas) 
     archivo2.close() 
 
-# invertir_lineas ('Pruebas Practica 8.txt') 
+# invertir_lineas ('Pruebas - Practica 8.txt') 
 
 #* Ejercicio 4. 
 def agregar_frase_al_final (nombre_archivo:str , frase:str) -> None : 
@@ -207,7 +210,7 @@ def agregar_frase_al_final (nombre_archivo:str , frase:str) -> None :
     archivo2.writelines(lineas) 
     archivo2.close() 
 
-# agregar_frase_al_final ('Pruebas Práctica 8.txt','El camino asi es.') 
+# agregar_frase_al_final ('Pruebas - Practica 8.txt','El camino asi es.') 
 
 #* Ejercicio 5. 
 def agregar_frase_al_principio (nombre_archivo:str , frase:str) -> None :
@@ -223,7 +226,7 @@ def agregar_frase_al_principio (nombre_archivo:str , frase:str) -> None :
     archivo.writelines(lineas2) 
     archivo.close() 
 
-# agregar_frase_al_principio ('Pruebas Práctica 8 (2).txt','El camino así es. ') 
+# agregar_frase_al_principio ('Pruebas - Practica 8 (2).txt','El camino así es. ') 
 
 
 #* Ejercicio 6.
@@ -254,3 +257,169 @@ def listar_palabras_de_archvio (nombre_archivo:str) -> list[str] :
 
 
 #* Ejercicio 7.
+def pertenece (s:list[str] , e:str) : 
+    res = False
+    i:int = 0 
+    while (i < len(s)) :
+        if (s[i] == e) :
+            res = True 
+            i += 1
+        else : 
+            i += 1
+    return res 
+
+def posicion_de_la_primer_coma (l:str) -> int : 
+    i:int = 0 
+    while (l[i] != ',') : 
+        i += 1 
+    return i 
+
+def buscador_alumno (li:str , lu:str) -> bool :
+        primer_coma:int = posicion_de_la_primer_coma (li)
+        lu_actual:str = '' 
+        for i in range (0,primer_coma,1) :  
+            if ((li[i]) != ',') : 
+                lu_actual = lu_actual + li[i] 
+            else : 
+                None 
+        if (lu_actual == lu) : 
+            lu_actual = ''
+            return True 
+        else : 
+            lu_actual = ''
+            return False 
+        
+def nota_de_linea (lin:str) -> float : 
+        nota:str =''
+        contador_de_comas:int = 0 
+        for i in range (0,len(lin),1) : 
+            if ((lin[i] == ',') and (contador_de_comas < 3)) : 
+                contador_de_comas = contador_de_comas + 1 
+            elif (contador_de_comas == 3) : 
+                nota = nota + lin[i] 
+            elif (lin[i] == '\n') : 
+                None 
+            else : 
+                None 
+        return float(nota)
+
+def notas_de_estudiante (lineas:list[str] , lu:str) -> list[float] : 
+        notas:list[float] = [] 
+        for i in range (0,len(lineas),1) : 
+            if ((buscador_alumno (lineas[i],lu)) == True) : 
+                notas.append(nota_de_linea(lineas[i]))
+            else : 
+                None 
+        return notas
+
+def promedio_estudiante (nombre_archivo:str , lu:str) -> float : 
+    archivo = open(nombre_archivo,'r')
+    lineas:list[str] = archivo.readlines() 
+    archivo.close()  
+    suma_notas:float = 0 
+    notas:list[float] = notas_de_estudiante(lineas,lu)
+    i:int = 0 
+    while (i < len(notas)) :
+        suma_notas = suma_notas + notas[i] 
+        i += 1 
+    promedio:float = suma_notas / len(notas) 
+    return promedio
+
+def lista_estudiantes (nombre_archivo:str) -> list[str] : 
+    archivo = open(nombre_archivo,'r')
+    lineas:list[str] = archivo.readlines() 
+    archivo.close()
+    lu_estudiantes:list[str] = [] 
+    estudiante_actual:str = ''
+    for i in range (1,len(lineas),1) : 
+        for j in range (0,posicion_de_la_primer_coma(lineas[i]),1) :
+            estudiante_actual = estudiante_actual +  lineas[i][j] 
+        if ((pertenece (lu_estudiantes,estudiante_actual)) == False) :
+            lu_estudiantes.append(estudiante_actual) 
+        estudiante_actual = ''
+    return lu_estudiantes
+
+def calcular_promedio_por_estudiante (nombre_archivo_notas:str , nombre_archivo_promedios:str) -> None :
+    archivo1 = open (nombre_archivo_notas,'r')
+    lineas_notas:list[str] = archivo1.readlines() 
+    archivo1.close() 
+
+    lista_de_estudiantes:list[str] = lista_estudiantes (nombre_archivo_notas)
+    promedios:list[float] = [] 
+    for i in range (0,len(lista_de_estudiantes),1) :
+        promedios.append(promedio_estudiante (nombre_archivo_notas,lista_de_estudiantes[i])) 
+    
+    lista_escritura:list[str] = ['LU de alumno,Promedio de Notas de la Carrera\n']
+    for i in range (0,len(promedios),1) : 
+        lista_escritura.append (str(str(lista_de_estudiantes[i]))+','+(str(promedios[i]))+'\n') 
+    
+    archivo2 = open (nombre_archivo_promedios,'w')
+    archivo2.writelines(lista_escritura)
+    archivo2.close()
+
+# calcular_promedio_por_estudiante ('Ejercicio 7 - Practica 8.csv','Ejercicio 7 - Practica 8 (2).csv') 
+
+# Tres horas estuve diseñando esto, estoy explotado. Pero funciona como quiero :) (capaz no entendí bien
+# la consigna y no funciona como debe, PERO SI COMO QUIERO).
+
+
+#* Ejercicio 8. 
+def generar_nros_al_azar (cantidad:int , desde:int , hasta:int) -> Pila[int] :
+    p:Pila[int] = Pila() 
+    i:int = 1 
+    while (i <= cantidad) : 
+        numero:int = random.randint(desde,hasta) 
+        p.put(numero) 
+        i += 1
+    # print (p.queue)  --> para verificar quién es la lista 'p'.
+    return p 
+
+# print (generar_nros_al_azar (3,0,8)) 
+# print (generar_nros_al_azar (5,-8,7))
+
+
+#* Ejercicio 9. 
+def cantidad_elementos (p:Pila) -> int : 
+    elementos:list = [] 
+    while ((p.empty()) == False) : 
+        elementos.append(p.get()) 
+    cantidad:int = len(elementos) 
+    for i in range (((len(elementos))-1),-1,-1) : 
+        p.put(elementos[i]) 
+    print(p.queue)
+    return cantidad 
+
+# p_vacía = Pila() 
+# p_con_cosas = Pila()
+# p_con_cosas.put(_)
+# p_con_cosas.put(_)
+# p_con_cosas.put(_)
+# print (cantidad_elementos(p_vacía))
+# # print (cantidad_elementos(p_con_cosas)) 
+
+
+#* Ejercicio 10.
+def buscar_el_maximo (p:Pila[int]) -> int :
+    elementos:list[int] = [] 
+    maximo:int = p.get()
+    elementos.append(maximo) 
+    while ((p.empty()) == False) : 
+        candidato:int = p.get()
+        if (maximo <= candidato) : 
+            maximo = candidato 
+            elementos.append(maximo)  
+        else : 
+            elementos.append(candidato) 
+    for i in range (((len(elementos))-1),-1,-1) :
+        p.put(elementos[i]) 
+    print (p.queue)
+    return maximo 
+
+# p = Pila()
+# p.put(_)
+# p.put(_)
+# p.put(_)
+# print (buscar_el_maximo(p)) 
+
+
+#* Ejercicio 
